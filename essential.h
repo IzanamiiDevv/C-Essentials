e5edd3accfb5a903e4a202dc086ac6f79766cec3
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-12 23:16:57 +0800

    essential header lib

---
+++ essential.h
@@ -0,0 +1,32 @@
+#ifndef ESSENTIAL
+#define ESSENTIAL
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//User Input
+int get_int() {
+
+}
+
+float get_float() {
+
+}
+
+char* get_string() {
+
+}
+
+bool get_bool() {
+
+}
+
+double get_double() {
+
+}
+
+long get_long() {
+
+}
+
+#endif //ESSENTIAL
