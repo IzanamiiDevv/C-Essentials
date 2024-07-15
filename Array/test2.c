c3eab982bca48b0302c92f2f931955bc8039bafa
IzanamiiDevv <rafaeloli596@gmail.com>
2024-07-15 20:20:45 +0800

    Add Code

--- Array/test2.c
+++ Array/test2.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define CREDIT "IzanamiiDev"
 typedef struct {
     void* list;
     unsigned int len;
-    size_t size;
 } Array;
 
-Array _MacroDefined(size_t size);
-#define Array_Init(type) (_MacroDefined(sizeof(type)));
-
-Array _MacroDefined(size_t size) {
+Array Array_Init() {
     Array buff;
     buff.len = 0;
-    buff.list = malloc(size);
-    buff.size = size;
+    buff.list = NULL;
     return buff;
 }
 
@@ -22,26 +19,36 @@ Array _MacroDefined(size_t size) {
 void Array_Free(Array* arr) {
     free(arr->list);
     arr->len = 0;
-    arr->size = 0;
 }
 
-#define Array_PushBack(arr, value ,type) { \
+#define Array_PushBack(arr, type, value) { \
     void* new_list; \
-    arr.len += 1; \
-    new_list = realloc(arr.list, arr.size * arr.len); \
+    (arr).len += 1; \
+    new_list = realloc((arr).list, sizeof(type) * (arr).len); \
     if (new_list == NULL) return 1; \
-    arr.list = new_list; \
-    ((type*)arr.list)[arr.len - 1] = value; \
+    (arr).list = new_list; \
+    ((type*)(arr).list)[(arr).len - 1] = value; \
 };
+
+#define Array_Print(format, type, arr) { \
+    printf("["); \
+    for(int i = 0; i < (arr).len; i++) { \
+        printf(format, ((type*)(arr).list)[i]); \
+        if(i == (arr).len - 1) break; \
+        printf(", "); \
+    } \
+    printf("] \n"); \
+}
 
 
 int main() {
-    Array myarr = Array_Init(char*);
-    Array_PushBack(myarr, "Hello", char* );
-    Array_PushBack(myarr, "World", char* );
-    for(int i = 0; i < myarr.len; i++) {
-        printf("%s", ((char**)myarr.list)[i]);
-    }
+    Array myarr = Array_Init();
+    Array_PushBack(myarr, char* , "Hello");
+    Array_PushBack(myarr, char* , "World");
+    Array_Print("%s", char*, myarr);
+
+    printf("%s \n", ((char**)myarr.list)[0]);
+
     Array_Free(&myarr);
     return 0;
 }
