df36ab745745b2240b25df88f46c1c50a7e3acb9
IzanamiiDevv <rafaeloli596@gmail.com>
2024-07-16 23:49:54 +0800

    add Array Shuffle and Array Modify and Array PopBack

--- Array/test2.c
+++ Array/test2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define CREDIT "IzanamiiDev"
 
@@ -22,14 +23,22 @@ void Array_Free(Array* arr) {
     arr->len = 0;
 }
 
-#define Array_PushBack(arr, type, value) { \
+#define Array_PushBack(type ,arr, value) { \
     void* new_list; \
     (arr).len += 1; \
     new_list = realloc((arr).list, sizeof(type) * (arr).len); \
-    if (new_list == NULL) return 1; \
+    if (new_list == NULL) printf("Error: Memorry Allocation Failed."); \
     (arr).list = new_list; \
     ((type*)(arr).list)[(arr).len - 1] = value; \
 };
+
+#define Array_PopBack(type, arr) { \
+    void* new_list; \
+    (arr).len -= 1; \
+    new_list = realloc((arr).list, sizeof(type) * (arr).len); \
+    if(new_list == NULL) printf("Error: Memorry Allocation Failed."); \
+    (arr).list = new_list; \
+}
 
 #define Array_Print(format, type, arr) { \
     printf("["); \
@@ -58,13 +67,33 @@ void Array_Free(Array* arr) {
     } \
 }
 
+#define Array_Modify(type, arr, index, value) (( type* )(arr).list)[index] = value;
+
+#define Array_Shuffle(type, arr) { \
+    srand(time(0)); \
+    const unsigned int MAX = (arr).len - 1; \
+    const unsigned short MIN = 0; \
+    for(int i = 0; i < (arr).len; i++) {\
+        type temp = (( type* )(arr).list)[i]; \
+        int randn = (rand() % (MAX - MIN + 1)) + MIN; \
+        (( type* )(arr).list)[i] = (( type* )(arr).list)[randn];\
+        (( type* )(arr).list)[randn] = temp; \
+    } \
+}
+
 int main() {
     Array myarr = Array_Init();
-    Array_PushBack(myarr, char* , "Kamusta");
-    Array_PushBack(myarr, char* , "Hello World");
+    Array_PushBack(char*, myarr, "Kamusta");
+    Array_PushBack(char*, myarr, "Hello World");
+    Array_PushBack(char*, myarr, "Hii");
+    Array_Modify(char*, myarr, 0, "Test");
+    Array_Shuffle(char*, myarr);
+    Array_PopBack(char*, myarr);
 
-    printf("%s \n", Array_Take(char*, myarr, 0));
+    Array_Print("%s", char*, myarr);
 
     Array_Free(&myarr);
     return 0;
 }
+
+//(rand() % (MAX - MIN + 1)) + MIN
